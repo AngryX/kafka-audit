f144c0dd67334fdae4260ee9a3f13e2ac744e466
dkhomutov <dmitry.khomutov@infobip.com>
2018-10-08 13:59:32 +0300

    project structure and model description

---
+++ counters/src/main/kotlin/com/github/kafka/audit/model.kt
@@ -0,0 +1,38 @@
+package com.github.kafka.audit
+
+data class ApplicationData (
+        val applicationId: String = "",
+        val instanceId: String = "",
+        val locationId: String = ""
+)
+
+data class KafkaClientData(
+        val clientId: String = "",
+        val topicName: String,
+        val producer: Boolean = false
+)
+
+data class NumberOfMessages(
+        val application: ApplicationData,
+        val kafkaClient: KafkaClientData,
+        val counterType: String = "",
+        val intervalTimestamp: Long,
+        val value: Long = 0
+){
+
+    //todo: check keys and throw exception if there are not the same
+    operator fun plus(other: NumberOfMessages) = this.copy(value = value + other.value)
+
+    /*
+    override fun compareTo(other: NumberOfMessages) =
+            intervalTimestamp.compareTo(other.intervalTimestamp)
+
+     */
+
+}
+
+data class ProcessingResult(
+        val processorId: String = "",
+        val count: NumberOfMessages,
+        val processed: Boolean = true
+)
